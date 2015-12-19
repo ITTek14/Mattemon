01a98d6dcbe5bb59849ae2158a8ea07bb4a2ced1
TheMcDucky <TheMcDucky@hotmail.com>
2015-12-19 01:14:46 +0100

    YEAH

---
+++ src/org/ittek14/mattemon/GameState.java
@@ -0,0 +1,6 @@
+
+package org.ittek14.mattemon;
+
+public abstract class GameState {
+	public abstract void Update();
+}
---
+++ src/org/ittek14/mattemon/MainMenu.java
@@ -0,0 +1,9 @@
+package org.ittek14.mattemon;
+
+public class MainMenu extends GameState {
+
+	public void Update() {
+		
+	}
+	
+}
