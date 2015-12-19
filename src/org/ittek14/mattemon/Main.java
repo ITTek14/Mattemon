21a4859a37dc359a0e6558221f2af3fee8ad845a
TheMcDucky <TheMcDucky@hotmail.com>
2015-12-19 04:21:29 +0100

    aw yeah

--- src/org/ittek14/mattemon/Main.java
+++ src/org/ittek14/mattemon/Main.java
@@ -1,162 +1,40 @@
 package org.ittek14.mattemon;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.BasicGame;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
 
-import static org.lwjgl.glfw.GLFW.GLFW_FALSE;
-import static org.lwjgl.glfw.GLFW.GLFW_KEY_ESCAPE;
-import static org.lwjgl.glfw.GLFW.GLFW_RELEASE;
-import static org.lwjgl.glfw.GLFW.GLFW_RESIZABLE;
-import static org.lwjgl.glfw.GLFW.GLFW_TRUE;
-import static org.lwjgl.glfw.GLFW.GLFW_VISIBLE;
-import static org.lwjgl.glfw.GLFW.glfwCreateWindow;
-import static org.lwjgl.glfw.GLFW.glfwDefaultWindowHints;
-import static org.lwjgl.glfw.GLFW.glfwDestroyWindow;
-import static org.lwjgl.glfw.GLFW.glfwGetPrimaryMonitor;
-import static org.lwjgl.glfw.GLFW.glfwGetVideoMode;
-import static org.lwjgl.glfw.GLFW.glfwInit;
-import static org.lwjgl.glfw.GLFW.glfwMakeContextCurrent;
-import static org.lwjgl.glfw.GLFW.glfwPollEvents;
-import static org.lwjgl.glfw.GLFW.glfwSetErrorCallback;
-import static org.lwjgl.glfw.GLFW.glfwSetKeyCallback;
-import static org.lwjgl.glfw.GLFW.glfwSetWindowPos;
-import static org.lwjgl.glfw.GLFW.glfwSetWindowShouldClose;
-import static org.lwjgl.glfw.GLFW.glfwShowWindow;
-import static org.lwjgl.glfw.GLFW.glfwSwapBuffers;
-import static org.lwjgl.glfw.GLFW.glfwSwapInterval;
-import static org.lwjgl.glfw.GLFW.glfwTerminate;
-import static org.lwjgl.glfw.GLFW.glfwWindowHint;
-import static org.lwjgl.glfw.GLFW.glfwWindowShouldClose;
-import static org.lwjgl.opengl.GL11.GL_COLOR_BUFFER_BIT;
-import static org.lwjgl.opengl.GL11.GL_DEPTH_BUFFER_BIT;
-import static org.lwjgl.opengl.GL11.GL_TRIANGLES;
-import static org.lwjgl.opengl.GL11.glBegin;
-import static org.lwjgl.opengl.GL11.glClear;
-import static org.lwjgl.opengl.GL11.glClearColor;
-import static org.lwjgl.opengl.GL11.glColor3f;
-import static org.lwjgl.opengl.GL11.glEnd;
-import static org.lwjgl.opengl.GL11.glVertex3f;
-import static org.lwjgl.system.MemoryUtil.NULL;
+public class Main extends BasicGame {
 
-import org.lwjgl.glfw.GLFWErrorCallback;
-import org.lwjgl.glfw.GLFWKeyCallback;
-import org.lwjgl.glfw.GLFWVidMode;
-import org.lwjgl.opengl.GL;
-
-public class Main {
-	// Game States
-	private GameState currentState;
-	
-	private GameState mainMenu, inGame;
-	
-	 // We need to strongly reference callback instances.
-    private GLFWErrorCallback errorCallback;
-    private GLFWKeyCallback   keyCallback;
- 
-    // The window handle
-    private long window;
- 
-    public void run() {
-        try {
-            init();
-            loop();
- 
-            // Release window and window callbacks
-            glfwDestroyWindow(window);
-            keyCallback.release();
-        } finally {
-            // Terminate GLFW and release the GLFWErrorCallback
-            glfwTerminate();
-            errorCallback.release();
-        }
-    }
- 
-    private void init() {
-        // Setup an error callback. The default implementation
-        // will print the error message in System.err.
-        glfwSetErrorCallback(errorCallback = GLFWErrorCallback.createPrint(System.err));
- 
-        // Initialize GLFW. Most GLFW functions will not work before doing this.
-        if ( glfwInit() != GLFW_TRUE )
-            throw new IllegalStateException("Unable to initialize GLFW");
- 
-        // Configure our window
-        glfwDefaultWindowHints(); // optional, the current window hints are already the default
-        glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE); // the window will stay hidden after creation
-        glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE); // the window will be resizable
-
-        int HEIGHT = (int) (2160 / (1+Math.sqrt(5)));
-        int WIDTH = (int) (HEIGHT * (1+Math.sqrt(5)) / 2);
-        
-        // Create the window
-        window = glfwCreateWindow(WIDTH, HEIGHT, "Hello World!", NULL, NULL);
-        if ( window == NULL )
-            throw new RuntimeException("Failed to create the GLFW window");
- 
-        
-        // Setup a key callback. It will be called every time a key is pressed, repeated or released.
-        glfwSetKeyCallback(window, keyCallback = new GLFWKeyCallback() {
-            @Override
-            public void invoke(long window, int key, int scancode, int action, int mods) {
-                if ( key == GLFW_KEY_ESCAPE && action == GLFW_RELEASE )
-                    glfwSetWindowShouldClose(window, GLFW_TRUE); // We will detect this in our rendering loop
-            }
-        });
- 
-        // Get the resolution of the primary monitor
-        GLFWVidMode vidmode = glfwGetVideoMode(glfwGetPrimaryMonitor());
-        // Center our window
-        glfwSetWindowPos(
-            window,
-            (vidmode.width() - WIDTH) / 2,
-            (vidmode.height() - HEIGHT) / 2
-        );
- 
-        // Make the OpenGL context current
-        glfwMakeContextCurrent(window);
-        // Enable v-sync
-        glfwSwapInterval(1);
- 
-        // Make the window visible
-        glfwShowWindow(window);
-        
-        mainMenu = new MainMenu();
-        currentState = mainMenu;
-    }
- 
-    private void loop() {
-        // This line is critical for LWJGL's interoperation with GLFW's
-        // OpenGL context, or any context that is managed externally.
-        // LWJGL detects the context that is current in the current thread,
-        // creates the GLCapabilities instance and makes the OpenGL
-        // bindings available for use.
-        GL.createCapabilities();
-        
-        // Set the clear color
-        glClearColor(1.0f, 0.0f, 0.0f, 0.0f);
- 
-        // Run the rendering loop until the user has attempted to close
-        // the window or has pressed the ESCAPE key.
-        while ( glfwWindowShouldClose(window) == GLFW_FALSE ) {
-            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // clear the framebuffer
-
-    		glColor3f(0, 0, 0);
-    		glBegin(GL_TRIANGLES);
-    		glVertex3f(0, 1,-1);
-    		glVertex3f(1, 0,-1);
-    		glVertex3f(1, 1,-1);
-    		glEnd();
-    		
-            currentState.render();
-            
-            glfwSwapBuffers(window); // swap the color buffers
-
-            currentState.update();
-            
-            // Poll for window events. The key callback above will only be
-            // invoked during this call.
-            glfwPollEvents();
-        }
+	public Main(String gamename) {
+		super(gamename);
 	}
-	
+
+	@Override
+	public void init(GameContainer gc) throws SlickException {
+	}
+
+	@Override
+	public void update(GameContainer gc, int i) throws SlickException {
+	}
+
+	@Override
+	public void render(GameContainer gc, Graphics g) throws SlickException {
+		g.drawString("Howdy!", 10, 10);
+	}
+
 	public static void main(String[] args) {
-		new Main().run();
+		try {
+			AppGameContainer appgc;
+			appgc = new AppGameContainer(new Main("Simple Slick Game"));
+			appgc.setDisplayMode(640, 480, false);
+			appgc.start();
+		} catch (SlickException ex) {
+			Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+		}
+		
 	}
 }
--- src/org/ittek14/mattemon/MainMenu.java
+++ src/org/ittek14/mattemon/MainMenu.java
@@ -1,17 +1,8 @@
 package org.ittek14.mattemon;
 
-import org.lwjgl.glfw.GLFW;
-import org.lwjgl.opengl.GL11;
-import org.lwjgl.stb.STBImage;
-
-import static org.lwjgl.glfw.GLFW.*;
-import static org.lwjgl.opengl.GL11.*;
-import static org.lwjgl.system.MemoryUtil.*;
-
 public class MainMenu extends GameState {
 	
 	public void update() {
-		glGenTextures();
 	}
 
 	public void render() {
