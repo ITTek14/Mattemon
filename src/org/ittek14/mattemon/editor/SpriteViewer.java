29f66f8f5d7e1b8a3eeb0e9ec2d680157dda0523
TheMcDucky <TheMcDucky@hotmail.com>
2015-12-21 16:08:40 +0100

    asdf

--- src/org/ittek14/mattemon/editor/SpriteViewer.java
+++ src/org/ittek14/mattemon/editor/SpriteViewer.java
@@ -1,20 +1,33 @@
 package org.ittek14.mattemon.editor;
 
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.io.File;
+import java.io.FilenameFilter;
+import java.util.ArrayList;
+import java.util.Arrays;
 
-import org.ittek14.mattemon.Game;
 import org.ittek14.mattemon.gui.GUIButton;
 import org.ittek14.mattemon.gui.GUIContainer;
+import org.newdawn.slick.Animation;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Image;
 import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
+import org.newdawn.slick.SpriteSheet;
 import org.newdawn.slick.geom.Rectangle;
 import org.newdawn.slick.state.GameState;
 import org.newdawn.slick.state.StateBasedGame;
 
+import com.sun.javafx.collections.ArrayListenerHelper;
+
 public class SpriteViewer implements GameState {
+  private GUIContainer gui;
+  
+  private SpriteSheet spriteSheet;
+  private Animation anim;
+  private float zoom = 1;
+  private float zoomedWidth, zoomedHeight;
+  
   @Override
   public void mouseClicked(int button, int x, int y, int clickCount) {
     // TODO Auto-generated method stub
@@ -47,8 +60,7 @@ public class SpriteViewer implements GameState {
 
   @Override
   public void mouseWheelMoved(int change) {
-    // TODO Auto-generated method stub
-
+    zoom = Math.max(zoom + zoom * (float)change / 1500, 0.01f);
   }
 
   @Override
@@ -149,8 +161,6 @@ public class SpriteViewer implements GameState {
 
   @Override
   public void enter(GameContainer container, StateBasedGame game) throws SlickException {
-    // TODO Auto-generated method stub
-
   }
 
   @Override
@@ -161,7 +171,7 @@ public class SpriteViewer implements GameState {
 
   @Override
   public void init(GameContainer container, StateBasedGame game) throws SlickException {
-    
+  
   }
 
   @Override
@@ -171,10 +181,16 @@ public class SpriteViewer implements GameState {
 
   @Override
   public void render(GameContainer container, StateBasedGame game, Graphics g) throws SlickException {
+    if(anim != null){
+      anim.draw(container.getWidth()/2 - zoomedWidth/2, container.getHeight()/2 - zoomedHeight/2, zoomedWidth, zoomedHeight);
+    }
+    gui.render(container, game, g);
   }
 
   @Override
   public void update(GameContainer container, StateBasedGame game, int delta) throws SlickException {
+    zoomedWidth = anim.getWidth() * zoom;
+    zoomedHeight = anim.getHeight() * zoom;
+    gui.update(container, game, delta);
   }
-
 }
